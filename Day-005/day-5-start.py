2141ad76d7d4bb913f7d91298a1559d2c385ea91
Mai-farahat <maifarahat00@gmail.com>
2024-10-20 17:43:48 +0300

    Add Day-005-Password-Generator

---
+++ Day-005/day-5-start.py
@@ -0,0 +1,14 @@
+#fruits = ["apple", "banana", "cherry"]
+#for fruit in fruits:
+  #print(fruit)
+  #print(fruit + " pie")
+#print(fruits)
+
+#For loop with range
+#for number in range(1, 11, 3):
+  #print(number)
+
+sum = 0
+for num in range(1, 101):
+  sum += num
+print(sum)
