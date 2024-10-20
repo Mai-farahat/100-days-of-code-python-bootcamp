64a3e2ad7394f5ff3b65e868ddb9f98df99b681f
Mai-farahat <maifarahat00@gmail.com>
2024-10-20 18:57:49 +0300

    Add Day-007-Hangman-Game

---
+++ Day-007/Day-7-Hangman-5-Start/main.py
@@ -0,0 +1,17 @@
+#Step 5
+from replit import clear
+import random
+
+#TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
+#Delete this line: word_list = ["ardvark", "baboon", "camel"]
+from hangman_words import word_list
+from hangman_art import stages, logo
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+end_of_game = False
+lives = 6
+
+#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
+
+print(logo)
